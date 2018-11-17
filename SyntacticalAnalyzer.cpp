10fa44f779bc42f80159ea89a34e3def8c05f106
John Nethery Salman <jsalman@blue.cs.sonoma.edu>
2018-11-16 16:54:01 -0800

    Fixed some small errors in some functions

--- SyntacticalAnalyzer.cpp
+++ SyntacticalAnalyzer.cpp
@@ -12,8 +12,8 @@ SyntacticalAnalyzer::SyntacticalAnalyzer (char * filename)
 	// we can skip a lot of error checking on files as watts already made them
 	string name = filename;
 	string project_name = name.substr (0, name.length()-3) + ".p2";
-	project_file.open (projec_name.c_str());
-	// program ();
+	project_file.open (project_name.c_str());
+	program ();
 }
 
 SyntacticalAnalyzer::~SyntacticalAnalyzer ()
@@ -37,6 +37,10 @@ void SyntacticalAnalyzer::write_project_exit (string _function_name) {
 }
 
 bool SyntacticalAnalyzer::is_action (string _token_name) {
+  string action_tokens [] = {"IF_T", "COND_T", "LISTOP_T", "CONS_T", "AND_T", "OR_T", "NOT_T",
+                                   "NUMBERP_T", "LISTP_T", "ZEROP_T", "NULLP_T", "STRINGP_T", "PLUS_T",
+                                   "MINUS_T", "DIV_T", "MULT_T", "MODULO_T", "ROUND_T", "EQUALTO_T",
+                                   "GT_T", "LT_T", "GTE_T", "LTE_T", "IDENT_T", "DISPLAY_T", "NEWLINE_T"};
   for (int i = 0; i < 26; i++) {
     if (action_tokens[i] == _token_name) {
       return true;
@@ -46,7 +50,12 @@ bool SyntacticalAnalyzer::is_action (string _token_name) {
 }
 
 bool SyntacticalAnalyzer::is_any_other (string _token_name) {
-  for (int i = 0; i < ; i++) {
+  string any_other_tokens [] = {"LPAREN_T", "IDENT_T", "NUMLIT_T", "STRLIT_T", "CONS_T", "IF_T",
+                                      "DISPLAY_T", "NEWLINE_T", "LISTOP_T", "AND_T", "OR_T","NOT_T",
+                                      "DEFINE_T", "NUMBERP_T", "LISTP_T", "ZEROP_T", "NULLP_T", "STRINGP_T",
+                                      "PLUS_T", "MINUS_T", "DIV_T", "MULT_T", "MODULO_T", "ROUND_T",
+                                      "EQUALTO_T", "GT_T", "LT_T", "GTE_T", "LTE_T", "QUOTE_T", "COND_T", "ELSE_T"};
+  for (int i = 0; i < 32; i++) {
     if (any_other_tokens[i] == _token_name) {
       return true;
     }
@@ -54,8 +63,4 @@ bool SyntacticalAnalyzer::is_any_other (string _token_name) {
   return false;
 }
 
-void SyntacticalAnalyzer::report_error() {
-  string message = "'" + lex->GetLexeme() + "'" + " unexpected";
-  lex->ReportError(message);
-  return;
-}
+
